fc2614e2064c67e4427253d17e69f22d5388b366
Anthony Acosta M <anthony082008@hotmail.es>
2023-10-13 15:21:15 -0500

    Added resolution to point 3, and my personal notes about the challenge

---
+++ NOTES.md
@@ -0,0 +1,43 @@
+## Clojure Challenge
+
+It was very exciting and fun journey trying to learn this new language, honestly it was my first touch to this kind of programming and concepts, so if you see any bad practices or bad code, please let me know, I would love to learn more about it. <3
+
+### How to run
+
+Basically I just ran all the codes in the REPL terminal, loading the files:
+
+
+#### Point 1
+
+I wrote a separated file for the first point, so you can run it like this:
+
+```clojure
+~/clojure-challenge$ clj
+Clojure 1.10.1
+
+user=> (load-file "invoice_conditions.clj")
+```
+
+#### Point 2
+
+Here, I wasn't able to achieve the goal of the challenge, but I was very close to it. I'll be very happy if you can give me some feedback about it.
+
+```clojure
+~/clojure-challenge$ clj -A:deps
+Clojure 1.10.1
+
+user=> (load-file "invoice_spec.clj")
+```
+
+#### Point 3
+
+Here I just ran the tests in the IntelliJ IDE, but you can run it like this:
+
+```clojure
+~/clojure-challenge$ clj -A:deps
+Clojure 1.10.1
+
+user=> (load-file "invoice_item.clj")
+```
+
+## Thank you!
--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -1,4 +1,5 @@
-(ns invoice-item)
+(ns invoice-item
+  (:require [clojure.test :refer :all]))
 
 (defn- discount-factor [{:invoice-item/keys [discount-rate]
                          :or                {discount-rate 0}}]
@@ -10,3 +11,27 @@
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
 
+(deftest test-subtotal
+  (testing "Basic case, no discount"
+    (is (= 100.0 (subtotal {:invoice-item/precise-quantity 10
+                          :invoice-item/precise-price 10}))))
+
+  (testing "100% discount rate"
+    (is (= 0.0 (subtotal {:invoice-item/precise-quantity 10
+                      :invoice-item/precise-price 10
+                      :invoice-item/discount-rate 100}))))
+
+  (testing "Basic case, with 10% discount"
+    (is (= 90.0 (subtotal {:invoice-item/precise-quantity 10
+                         :invoice-item/precise-price 10
+                         :invoice-item/discount-rate 10}))))
+
+  (testing "Fractional discount rates"
+    (is (= 99.5 (subtotal {:invoice-item/precise-quantity 10
+                           :invoice-item/precise-price 10
+                           :invoice-item/discount-rate 0.5}))))
+
+  (testing "Zero price"
+    (is (= 0.0 (subtotal {:invoice-item/precise-quantity 10
+                        :invoice-item/precise-price 0}))))
+  )
